120a83b88ce98f567b68eab044b8bbc48fe31942
czh2906106206-svg <czh2906106206@gmail.com>
2026-05-18 00:14:01 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.simpleai.app',
+  appName: '简单生图',
+  webDir: 'dist'
+};
+
+export default config;
